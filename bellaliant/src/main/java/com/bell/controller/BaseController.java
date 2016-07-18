018280519c48ce1fac3cfb90183e6c369300de61
vijayct2001 <vijay.bss@gmail.com>
2016-07-18 21:14:34 +0530

    Update BaseController.java
    Hygieia integration done

--- bellaliant/src/main/java/com/bell/controller/BaseController.java
+++ bellaliant/src/main/java/com/bell/controller/BaseController.java
@@ -20,7 +20,7 @@ public class BaseController {
 		model.addAttribute("message", "Welcome");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
-                // updated project properties details
+                // updated project properties details for hygieia integration
 		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return VIEW_INDEX;
 
